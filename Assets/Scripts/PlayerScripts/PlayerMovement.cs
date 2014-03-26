e6e4c3d7a2c035c68d910af53bd26180f530014c
donald93 <blargalicious@gmail.com>
2014-03-26 00:33:24 -0600

    Attempting to create movement, it's broken

---
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+		int MovementSpeed;
+
+
+		// Use this for initialization
+		void Start ()
+		{
+				MovementSpeed = 1;
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+
+				float HorizontalMovement = Input.GetAxis ("Horizontal");
+				float VerticalMovement = Input.GetAxis ("Vertical");
+
+				Vector3 MovementDirection = new Vector2 (HorizontalMovement, VerticalMovement);
+
+				rigidbody2D.velocity (MovementDirection);
+		}
+}
